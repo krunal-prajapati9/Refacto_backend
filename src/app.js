e979dcad3f49b771107798083707bddad6bf939d
krunal-prajapati9 <krunalprajapati9630@gmail.com0>
2025-06-03 21:33:59 +0530

    aded security

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 const express= require('express')
 const airoute =require('./routes/ai.routes')
 const cors = require('cors')
+const helmet = require('helmet');
 const app =express()
+app.use(cors())
+app.use(express.json())
 
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      fontSrc: ["'self'", 'data:'],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      connectSrc: ["*"], // for APIs
+      imgSrc: ["'self'", "data:"],
+    },
+  })
+);
 app.get("/", (req,res) => {
     res.send("Hey server is runnning successfully")
 })
 
-app.use(cors())
-app.use(express.json())
 
 app.use("/ai" , airoute)
 
