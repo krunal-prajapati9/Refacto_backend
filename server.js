818b0a1d6452541a5c61afd93c8161a193d1b5f3
krunal-prajapati9 <krunalprajapati9630@gmail.com0>
2025-06-03 21:16:45 +0530

    env port aded

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
 const app = require("./src/app");
 
+const PORT = process.env.PORT || 3000; 
 
-
-app.listen(3000, () => {
-  console.log("servver is running on port 3000");
-  
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
